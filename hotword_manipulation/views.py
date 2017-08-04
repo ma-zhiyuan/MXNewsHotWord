299f707885484e83eb15c36d4480f6bdcc5afc75
zhiyuan.ma <zhiyuan.ma@zenjoy.net>
2017-08-04 11:14:48 +0800

    Add run shell

--- hotword_manipulation/views.py
+++ hotword_manipulation/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from elasticsearch import Elasticsearch,RequestsHttpConnection
-from elasticsearch import helpers
 
 es = Elasticsearch(hosts=[{'host':'search-mx-news-online-dev-vgoagyua54cwn35gjb5acqlroi.ap-northeast-2.es.amazonaws.com', 'port': 443}],
 connection_class=RequestsHttpConnection,
@@ -11,5 +9,5 @@ verify_certs=True)
 def index(request):
     query_string = "{\"query\": {\"match_all\": {}}}"
     res = es.search(index="hotword", body=query_string)
-
+    print res
     return render(request,'index.html',{'hotword':res})
