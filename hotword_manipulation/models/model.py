493f305c228f2f00d74be929ae9a41f0f941fab5
zhiyuan.ma <zhiyuan.ma@zenjoy.net>
2017-08-08 14:29:06 +0800

    Test pull

--- hotword_manipulation/models/model.py
+++ hotword_manipulation/models/model.py
@@ -1,25 +1,28 @@
 #-*- coding: UTF-8 -*-
 import logging
+import hashlib
+import time
+import json
 from elasticsearch import Elasticsearch,RequestsHttpConnection
 
 logger=logging.getLogger("django")
 
-class HotWordModel(object):
+class HotWordModel:
     def __init__(self):
         self._es_host='search-mx-news-online-dev-vgoagyua54cwn35gjb5acqlroi.ap-northeast-2.es.amazonaws.com'
         self._es_port=443
 
-    #def setES_CLIENT(self):
-    #   self._es_client = Elasticsearch(
-    #      hosts=[{'host': self._es_host, 'port': self._es_port}],
-    #      use_ssl=True,
-    #      verify_certs=True,
-    #      connection_class=RequestsHttpConnection
-    #    )
+    def setES_CLIENT(self):
+       self._es_client = Elasticsearch(
+          hosts=[{'host': self._es_host, 'port': self._es_port}],
+          use_ssl=True,
+          verify_certs=True,
+          connection_class=RequestsHttpConnection
+        )
 
     def query_item(self):
         self.setES_CLIENT()
-        query_top = "{\"query\": {\"match\": {\"valid\": 0}},\"sort\": {\"position\": {\"order\": \"asc\"}}}"
+        query_top = '{"query":{"match":{"valid":0}},"sort":[{"position":{"order":"asc"}},{"query_type":{"order":"desc"}},{"date_str":{"order":"desc"}}]}'
         query_black = "{\"query\": {\"match\": {\"valid\": 1}}}"
         top = self._es_client.search(index="hotword", body=query_top)
         black = self._es_client.search(index="hotword", body=query_black)
@@ -34,6 +37,27 @@ class HotWordModel(object):
         return topResultList,blackResultList
 
     def pullblack(self,queryword):
-        self.setES_CLIENT()
-        pullstring = ""
+        #self.setES_CLIENT()
+        word = Word()
+        queryjson = '{"query":{"match":{"query":"'+queryword+'"}}}'
+        wordjson = self._es_client.search(index="hotword", body=queryjson).get("hits").get("hits")[0].get("_source")
+        word.__dict__ = json.loads(wordjson)
+        word.valid = 1
+        m = hashlib.md5()
+        m.update(queryword)
+        md5word = m.hexdigest()
+        word.date = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+        word.date_str = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        pullstring =json.dumps(word.__dict__)
+        self._es_client.index(index="hotword", doc_type="hotword", id=md5word, body=pullstring)
+
+
+class Word:
+    def __init__(self):
+        self.query = ''
+        self.query_type = ''
+        self.position = ''
+        self.date = ''
+        self.date_str = ''
+        self.valid = ''
 
