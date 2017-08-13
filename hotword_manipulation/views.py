f11b1fe8ccbe5098816342ffbe416a1cb0db8057
ma-zhiyuan <maazhiyuan@gmail.com>
2017-08-13 12:17:09 +0800

    delete test data

--- hotword_manipulation/views.py
+++ hotword_manipulation/views.py
@@ -8,8 +8,6 @@ from hotword_manipulation.models.model import HotWordModel
 def index(request):
     hotWord = HotWordModel()
     topResultList,blackResultList = hotWord.query_item()
-    #topResultList = [{u'query_type': u'alphago', u'valid': 0, u'position': 1, u'date': u'2017-08-03', u'query': u'query1', u'date_str': u'2017-08-03 11:55:01'}, {u'query_type': u'alphago', u'valid': 0, u'position': 2, u'date': u'2017-08-03', u'query': u'query2', u'date_str': u'2017-08-03 11:55:02'}, {u'query_type': u'alphago', u'valid': 0, u'position': 3, u'date': u'2017-08-03', u'query': u'query3', u'date_str': u'2017-08-03 11:55:03'}, {u'query_type': u'alphago', u'valid': 0, u'position': 4, u'date': u'2017-08-03', u'query': u'query4', u'date_str': u'2017-08-03 11:55:04'}, {u'query_type': u'alphago', u'valid': 0, u'position': 5, u'date': u'2017-08-03', u'query': u'query5', u'date_str': u'2017-08-03 11:55:05'}, {u'query_type': u'alphago', u'valid': 0, u'position': 6, u'date': u'2017-08-03', u'query': u'query6', u'date_str': u'2017-08-03 11:55:06'}, {u'query_type': u'alphago', u'valid': 0, u'position': 7, u'date': u'2017-08-03', u'query': u'query7', u'date_str': u'2017-08-03 11:55:07'}, {u'query_type': u'alphago', u'valid': 0, u'position': 8, u'date': u'2017-08-03', u'query': u'query8', u'date_str': u'2017-08-03 11:55:08'}, {u'query_type': u'alphago', u'valid': 0, u'position': 9, u'date': u'2017-08-03', u'query': u'query9', u'date_str': u'2017-08-03 11:55:09'}]
-    #blackResultList = [{u'query_type': u'alphago', u'valid': 1, u'position': 10, u'date': u'2017-08-03', u'query': u'query10', u'date_str': u'2017-08-03 11:55:10'}]
     return render(request,'index.html',{'topResultList':topResultList,'blackResultList':blackResultList})
 
 def pullblack(request,queryword):
