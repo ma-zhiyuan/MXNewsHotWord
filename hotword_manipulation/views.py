c45caab1008b7f2ac24e224c532a06e2f67ab2c1
zhiyuan_ma <ma-zhiyuan@users.noreply.github.com>
2017-09-11 23:36:41 +0800

    add notes

--- hotword_manipulation/views.py
+++ hotword_manipulation/views.py
@@ -4,12 +4,13 @@ import time
 from django.shortcuts import render, redirect
 from hotword_manipulation.models.model import HotWordModel
 
-
+#默认入口
 def index(request):
     hotWord = HotWordModel()
     topResultList,blackResultList = hotWord.query_item()
     return render(request,'index.html',{'topResultList':topResultList,'blackResultList':blackResultList})
 
+#拉黑接口
 def pullblack(request,queryword):
     hotWord = HotWordModel()
     hotWord.pullblack(queryword)
@@ -17,6 +18,7 @@ def pullblack(request,queryword):
     topResultList,blackResultList = hotWord.query_item()
     return render(request,'index.html',{'topResultList':topResultList,'blackResultList':blackResultList})
 
+#恢复
 def recovery(request,queryword):
     hotWord = HotWordModel()
     hotWord.recovery(queryword)
@@ -24,6 +26,7 @@ def recovery(request,queryword):
     topResultList, blackResultList = hotWord.query_item()
     return render(request, 'index.html', {'topResultList': topResultList, 'blackResultList': blackResultList})
 
+#插入热词
 def insertword(request):
     hotWord = HotWordModel()
     if request.POST:
